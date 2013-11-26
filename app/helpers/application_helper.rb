a1339084ed765ec98e01b8b6879b814fbe5f1791
tam <tam@tam.(none)>
2013-11-26 11:09:49 +0700

    update function ajax for left menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,16 +10,16 @@ module ApplicationHelper
   def puts_name group, count
     result = ""
     if group.children.empty?
-      result += "<tr><th><a id='group_#{ group.id }' href='#'>#{ group.name }</a></th></tr>"
+      result += "<tr><th class='item_left_menu'><a id='group_#{ group.id }' href='#'>#{ group.name }</a></th></tr>"
       group.teams.each do |team|
-        result += "<tr><td><a id='team_#{ team.id }' href='#'>#{ team.name }</a></td></tr>"
+        result += "<tr><td class='item_left_menu'><a id='team_#{ team.id }' href='#'>#{ team.name }</a></td></tr>"
       end
       return result
     else
-      result = "<tr><th><a id='group_#{ group.id }' href='#'>#{ group.name }</a></th></tr>"
+      result = "<tr><th class='item_left_menu'><a id='group_#{ group.id }' href='#'>#{ group.name }</a></th></tr>"
       count += 1
       group.teams.each do |team|
-        result += "<tr><td><a id='team_#{ team.id }' href='#'>#{ team.name }</a></td></tr>"
+        result += "<tr><td class='item_left_menu'><a id='team_#{ team.id }' href='#'>#{ team.name }</a></td></tr>"
       end
       group.children.each do |child|
         result += puts_name(child, count)
