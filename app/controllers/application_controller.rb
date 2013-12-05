044afced3e3662dfccec372ab62fd8612e985cea
Ban <le.van.ban@framgia.com>
2013-12-05 13:22:39 +0700

    fixed #9682 fix bug show access denied when, show notice login fail, new css for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,17 @@ class ApplicationController < ActionController::Base
     redirect_to root_url
   end
   layout Proc.new { |controller| controller.devise_controller? ? 'signin' : 'application' }
+
+private
+  def after_sign_out_path_for(resource)
+    session[:previous_url] || new_user_session_path
+  end
+
+  def sign_out(resource_or_scope=nil)
+    if session.empty?
+      return
+    else
+      super
+    end
+  end  
 end
