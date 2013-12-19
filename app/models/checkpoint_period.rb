c788022c16e043011fd45dd31e38b4592a7b7dfb
Ban <le.van.ban@framgia.com>
2013-12-19 17:38:45 +0700

    refs #10018 [Create period] - Add select question when create period

--- app/models/checkpoint_period.rb
+++ app/models/checkpoint_period.rb
@@ -1,14 +1,29 @@
 class CheckpointPeriod < Base
   has_many :checkpoints, dependent: :destroy
+  has_many :checkpoint_questions
+  has_many :period_questions
+  has_many :checkpoint_questions, through: :period_questions, source: :checkpoint_question
+
+  accepts_nested_attributes_for :period_questions, allow_destroy: true, reject_if: :all_blank
 
   validates :name, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-
-  has_many :period_questions
-  has_many :checkpoint_questions, through: :period_questions, source: :checkpoint_question
+  validates :period_questions, presence: true
 
   def toString
     "#{self.name}(#{self.start_date}~#{self.end_date})"
+  end
+
+  def build_period_question checkpoint_question_id
+    self.period_questions.where(checkpoint_question_id: checkpoint_question_id).presence ||
+      self.period_questions.build(checkpoint_question_id: checkpoint_question_id)
+  end
+private
+  class << self
+    def updatable_attrs
+      [:name, :start_date, :end_date,
+        period_questions_attributes: [:id, :_destroy, :checkpoint_question_id, :checkpoint_period_id]]
+    end
   end
 end
--- app/models/period_question.rb
+++ app/models/period_question.rb
@@ -1,4 +1,9 @@
-class PeriodQuestion < ActiveRecord::Base
+class PeriodQuestion < Base
   belongs_to :checkpoint_period
   belongs_to :checkpoint_question
+
+  validates :checkpoint_question_id, uniqueness: {scope: :checkpoint_period_id}, presence: true
+
+  scope :by_type_question, ->parent_id {joins(:checkpoint_question).where("checkpoint_questions.checkpoint_type = ?", parent_id) unless parent_id.nil? }
+  scope :get_parent_question, ->parent_id {joins(:checkpoint_question).where("checkpoint_questions.parent_id = ?", parent_id) unless parent_id.nil? }
 end
